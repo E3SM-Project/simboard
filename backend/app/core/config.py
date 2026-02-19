a0e15dccea80ff4d020a613195ca82ba4f46df89
Vo <tomvothecoder@gmail.com>
2026-02-19 13:00:11 -0800

    Add script to create admin account with domain env var

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -61,6 +61,7 @@ class Settings(BaseSettings):
     # ----------------------------------------
     env: str = "development"
     port: int = 8000
+    domain: str = "127.0.0.1"
 
     # Frontend
     # ----------------------------------------
